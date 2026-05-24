5fbb0cfd8e32d643a559331601c0acd0e161575f
Yotam Alon <yotam.alon@axonius.com>
2026-05-24 08:18:13 +0300

    Add missing include

--- src/cow_types/cow_utils.h
+++ src/cow_types/cow_utils.h
@@ -3,6 +3,9 @@
 
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#if PY_VERSION_HEX < 0x030D0000
+#include <frameobject.h>
+#endif
 
 int cow_replace_in_frame(PyObject *old_obj, PyObject *new_obj);
 
